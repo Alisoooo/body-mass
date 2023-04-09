688c6d6893ea6705dd2905eaf386d7a6f7964657
Alisa <solomatinaalisa14@gmail.com>
2023-04-09 20:53:02 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BmiService service = new BmiService();
+        int result = service.calculate(50, 1.90);
+
+        System.out.println(result);
+       }
+    }
